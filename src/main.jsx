b2be99d51bea408a0a0df297c6cdbbc6b53f44b9
eodorfman111 <66503717+eodorfman111@users.noreply.github.com>
2025-12-01 16:29:36 -0500

    Refactor imports and update ChakraProvider usage

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import App from "./App";
+import "./index.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
